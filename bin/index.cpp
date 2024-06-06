36150b4450b2c1f089699aef67b5d86003950be5
deadnotxaa <aa.khudyakov@icloud.com>
2024-06-06 18:22:56 +0300

    feat: project structure initiated, QT, GTest and Protobuf libraries added

---
+++ bin/index.cpp
@@ -0,0 +1,13 @@
+#include "proto/.pb.h"
+#include "lib/Index/Index.hpp"
+//#include "lib/Search/Search.hpp"
+
+#include <QWidget>
+#include <QDebug>
+#include <QApplication>
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
---
+++ bin/search.cpp
@@ -0,0 +1,11 @@
+#include "proto/.pb.h"
+//#include "lib/Index/Index.hpp"
+#include "lib/Search/Search.hpp"
+
+#include <QWidget>
+#include <QDebug>
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
